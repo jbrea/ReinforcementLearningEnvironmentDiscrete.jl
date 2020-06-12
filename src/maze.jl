8aa12653a4a4fc35a6d0cce2c3757e4339e8fb0d
Johanni Brea <jbrea@users.noreply.github.com>
2020-06-12 19:10:45 +0200

    zero thickness walls

--- src/maze.jl
+++ src/maze.jl
@@ -1,130 +1,122 @@
-function getemptymaze(dimx, dimy)
-    maze = ones(Int, dimx, dimy)
-    maze[1,:] .= maze[end,:] .= 0
-    maze[:, 1] .= maze[:, end] .= 0
-    maze
+using SparseArrays, LinearAlgebra
+struct Maze
+    dimx::Int
+    dimy::Int
+    walls::Symmetric{Bool,SparseMatrixCSC{Bool,Int}}
+end
+function emptymaze(dimx, dimy)
+    Maze(dimx, dimy, Symmetric(sparse([], [], Bool[], dimx * dimy, dimx * dimy)))
+end
+iswall(maze, pos, dir) = hitborder(maze, pos, dir) || maze.walls[pos, nextpos(maze, pos, dir)]
+function setwall!(maze, pos, dir)
+    npos = nextpos(maze, pos, dir)
+    maze.walls.data[sort([pos, npos])...] = true
+end
+function nextpos(maze, pos, dir)
+    dir == :up && return pos - 1
+    dir == :down && return pos + 1
+    dir == :left && return pos - maze.dimy
+    dir == :right && return pos + maze.dimy
+end
+function hitborder(maze, pos, dir)
+    dir == :up && pos % maze.dimy == 1 && return true
+    dir == :down && pos % maze.dimy == 0 && return true
+    dir == :left && pos <= maze.dimy && return true
+    dir == :right && pos > maze.dimy * (maze.dimx - 1) && return true
+    return false
 end
 
-function setwall!(maze, startpos, endpos)
-    dimx, dimy = startpos - endpos
-    if dimx == 0
-        maze[startpos[1], startpos[2]:endpos[2]] .= 0
-    else
-        maze[startpos[1]:endpos[1], startpos[2]] .= 0
-    end
+function orthogonal_directions(dir)
+    dir ∈ (:up, :down) && return (:left, :right)
+    return (:up, :down)
 end
 
-function indto2d(maze, pos)
-    dimx = size(maze, 1)
-    [rem(pos, dimx), div(pos, dimx) + 1]
+function is_wall_neighbour(maze, pos)
+    for dir in (:up, :down, :left, :right)
+        iswall(maze, pos, dir) && return true
+    end
+    for npos in (nextpos(maze, pos, :up), nextpos(maze, pos, :down))
+        for dir in (:left, :right)
+            iswall(maze, npos, dir) && return true
+        end
+    end
+    return false
 end
-function posto1d(maze, pos)
-    dimx = size(maze, 1)
-    (pos[2] - 1) * dimx + pos[1]
+function is_wall_tangential(maze, pos, dir)
+    for ortho_dir in orthogonal_directions(dir)
+        iswall(maze, pos, ortho_dir) && return true
+    end
+    return false
+end
+is_wall_ahead(maze, pos, dir) = iswall(maze, pos, dir)
+
+function addrandomwall!(maze; rng = Random.GLOBAL_RNG)
+    potential_startpos = filter(x -> !is_wall_neighbour(maze, x), 1:maze.dimx * maze.dimy)
+    if potential_startpos == []
+        @warn("Cannot add a random wall.")
+        return maze
+    end
+    pos = rand(rng, potential_startpos)
+    direction = rand(rng, (:up, :down, :left, :right))
+    while true
+        setwall!(maze, pos, orthogonal_directions(direction)[2])
+        pos = nextpos(maze, pos, direction)
+        is_wall_tangential(maze, pos, direction) && break
+        if is_wall_ahead(maze, pos, direction)
+            setwall!(maze, pos, orthogonal_directions(direction)[2])
+            break
+        end
+    end
+    return maze
 end
 
-function checkpos(maze, pos)
-    count = 0
-    for dx in -1:1
-        for dy in -1:1
-            count += maze[(pos + [dx, dy])...] == 0
-        end
-    end
-    count
-end
-
-function addrandomwall!(maze)
-    startpos = rand(ENV_RNG, findall(x -> x != 0, maze[:]))
-    startpos = indto2d(maze, startpos)
-    starttouch = checkpos(maze, startpos)
-    if starttouch > 0
-        return 0
-    end
-    endx, endy = startpos
-    if rand(ENV_RNG, 0:1) == 0 # horizontal
-        while checkpos(maze, [endx, startpos[2]]) == 0
-            endx += 1
-        end
-        if maze[endx + 1, startpos[2]] == 1 &&
-            maze[endx + 1, startpos[2] + 1] ==
-            maze[endx + 1, startpos[2] - 1] == 0
-            endx -= 1
-        end
-    else
-        while checkpos(maze, [startpos[1], endy]) == 0
-            endy += 1
-        end
-        if maze[startpos[1], endy + 1] == 1 &&
-            maze[startpos[1] + 1, endy + 1] ==
-            maze[startpos[1] - 1, endy + 1] == 0
-            endx -= 1
-        end
-    end
-    setwall!(maze, startpos, [endx, endy])
-    return 1
-end
-
-function setTandR!(d)
-    for s in d.statefrommaze[findall(x -> x != 0, reshape(d.maze, :))]
-        setTandR!(d, s)
-    end
-end
-function setTandR!(d, s)
-    T = d.mdp.trans_probs
-    R = d.mdp.reward
-    statefrommaze = d.statefrommaze
-    goals = d.goals
-    ns = d.mdp.observationspace.n
-    maze = d.maze
-    for (aind, a) in enumerate(([0, 1], [1, 0], [0, -1], [-1, 0]))
-        pos = indto2d(maze, d.mazefromstate[s])
-        nextpos = maze[(pos + a)...] == 0 ? pos : pos + a
-        if d.neighbourstateweight > 0 && !(statefrommaze[posto1d(maze, nextpos)] in goals)
-            positions = []
-            push!(positions, nextpos)
-            weights = [1.]
-            for dir in ([0, 1], [1, 0], [0, -1], [-1, 0])
-                if maze[(nextpos + dir)...] != 0
-                    push!(positions, nextpos + dir)
-                    push!(weights, d.neighbourstateweight)
-                end
-            end
-            states = map(p -> statefrommaze[posto1d(maze, p)], positions)
-            weights /= sum(weights)
-            T[aind, s] = sparsevec(states, weights, ns)
-        else
-            nexts = statefrommaze[posto1d(maze, nextpos)]
-            T[aind, s] = sparsevec([nexts], [1.], ns)
-            if nexts in goals
-                R[aind, s] = d.goalrewards[findfirst(x -> x == nexts, goals)]
-            end
-        end
-    end
-end
-
-function isinsideframe(maze, i)
-    nx, ny = size(maze)
-    i > nx && i < nx * (ny - 1) && i % nx != 0 && i % nx != 1
-end
 function n_effective(n, f, list)
     N = n === nothing ? div(length(list), Int(1/f)) : n
     min(N, length(list))
 end
-function breaksomewalls!(m; f = 1/50, n = nothing)
-    zeros = Int[]
-    for i in 1:length(m)
-        m[i] == 0 && isinsideframe(m, i) && push!(zeros, i)
+function breaksomewalls!(m; f = 1/50, n = nothing, rng = Random.GLOBAL_RNG)
+    wall_idxs = findall(m.walls.data)
+    pos = sample(rng, wall_idxs, n_effective(n, f, wall_idxs), replace = false)
+    m.walls.data[pos] .= false
+    dropzeros!(m.walls.data)
+    m
+end
+# function addobstacles!(m; f = 1/100, n = nothing, rng = Random.GLOBAL_RNG)
+#     nz = findall(x -> x == 1, reshape(m, :))
+#     pos = sample(rng, nz, n_effective(n, f, nz), replace = false)
+#     m[pos] .= 0
+#     m
+# end
+setTandR!(d) = for s in 1:d.maze.dimx * d.maze.dimy setTandR!(d, s) end
+function setTandR!(d, s)
+    T = d.mdp.trans_probs
+    R = d.mdp.reward
+    goals = d.goals
+    ns = d.mdp.observationspace.n
+    maze = d.maze
+    for (aind, a) in enumerate((:up, :down, :left, :right))
+        npos = iswall(maze, s, a) ? s : nextpos(maze, s, a)
+        if d.neighbourstateweight > 0 && !(npos in goals)
+            positions = [npos]
+            weights = [1.]
+            for dir in (:up, :down, :left, :right)
+                if !iswall(maze, npos, dir)
+                    push!(positions, nextpos(maze, npos, dir))
+                    push!(weights, d.neighbourstateweight)
+                end
+            end
+            weights /= sum(weights)
+            T[aind, s] = sparsevec(positions, weights, ns)
+        else
+            T[aind, s] = sparsevec([npos], [1.], ns)
+            if s in goals
+                idx_goals = findfirst(x -> x == s, goals)
+                R[s] = d.goalrewards[idx_goals]
+            end
+        end
     end
-    pos = sample(ENV_RNG, zeros, n_effective(n, f, zeros), replace = false)
-    m[pos] .= 1
-    m
 end
-function addobstacles!(m; f = 1/100, n = nothing)
-    nz = findall(x -> x == 1, reshape(m, :))
-    pos = sample(ENV_RNG, nz, n_effective(n, f, nz), replace = false)
-    m[pos] .= 0
-    m
-end
+
 """
     struct DiscreteMaze
         mdp::MDP
@@ -135,11 +127,9 @@ end
 """
 struct DiscreteMaze{T}
     mdp::T
-    maze::Array{Int, 2}
+    maze::Maze
     goals::Array{Int, 1}
     goalrewards::Array{Float64, 1}
-    statefrommaze::Array{Int, 1}
-    mazefromstate::Array{Int, 1}
     neighbourstateweight::Float64
 end
 """
@@ -154,38 +144,39 @@ for the different goal states or constant reward for all goals), cost of moving
 a certain probability to a neighbouring state, where `neighbourstateweight`
 controls this probability.
 """
-function DiscreteMaze(; nx = 40, ny = 40, nwalls = div(nx*ny, 10), kwargs...)
-    m = getemptymaze(nx, ny)
-    [addrandomwall!(m) for _ in 1:nwalls]
-    breaksomewalls!(m)
-    DiscreteMaze(m; kwargs...)
+function DiscreteMaze(; nx = 40, ny = 40, nwalls = div(nx*ny, 20),
+                        rng = Random.GLOBAL_RNG, kwargs...)
+    m = emptymaze(nx, ny)
+    for _ in 1:nwalls
+        addrandomwall!(m, rng = rng)
+    end
+    breaksomewalls!(m, rng = rng)
+    DiscreteMaze(m; rng = rng, kwargs...)
 end
-function DiscreteMaze(maze; ngoals = 1, goalrewards = 1., stepcost = 0,
-                      stochastic = false,
-                      neighbourstateweight = stochastic ? .05 : 0.,
-                      compressed = true)
+function DiscreteMaze(maze; ngoals = 1,
+                            goalrewards = 1.,
+                            stepcost = 0,
+                            stochastic = false,
+                            neighbourstateweight = stochastic ? .05 : 0.,
+                            rng = Random.GLOBAL_RNG)
     na = 4
-    nzpos = findall(x -> x != 0, reshape(maze, :))
-    statefrommaze = compressed ? cumsum(reshape(maze, :)) : collect(1:length(maze))
-    mazefromstate = compressed ? nzpos : collect(1:length(maze))
-    legalstates = statefrommaze[nzpos]
-    ns = length(mazefromstate)
+    ns = maze.dimx * maze.dimy
+    legalstates = 1:ns
     T = Array{SparseVector{Float64,Int}}(undef, na, ns)
-    goals = sort(sample(ENV_RNG, legalstates, ngoals, replace = false))
+    goals = sort(sample(rng, legalstates, ngoals, replace = false))
     R = -ones(na, ns) * stepcost
     isterminal = zeros(Int, ns); isterminal[goals] .= 1
     isinitial = setdiff(legalstates, goals)
     res = DiscreteMaze(MDP(DiscreteSpace(ns, 1),
                            DiscreteSpace(na, 1),
-                           rand(ENV_RNG, legalstates),
+                           rand(rng, legalstates),
                            T, R,
-                           isinitial, isterminal),
+                           isinitial,
+                           isterminal),
                        maze,
                        goals,
                        typeof(goalrewards) <: Number ? fill(goalrewards, ngoals) :
                                                        goalrewards,
-                       statefrommaze,
-                       mazefromstate,
                        neighbourstateweight)
     setTandR!(res)
     res
@@ -294,9 +285,24 @@ plotenv(env::Union{ChangeDiscreteMaze,RandomChangeDiscreteMaze}) = plotenv(env.d
 
 function plotenv(env::DiscreteMaze)
     goals = env.goals
-    mazefromstate = env.mazefromstate
-    m = deepcopy(env.maze)
-    m[mazefromstate[goals]] .= 3
-    m[mazefromstate[env.mdp.state]] = 2
-    imshow(m, colormap = 21, size = (400, 400))
+    px = 6
+    m = zeros(px*env.maze.dimx, px*env.maze.dimy)
+    for i in 1:env.maze.dimx
+        for j in 1:env.maze.dimy
+            s = (i - 1) * env.maze.dimy + j
+            iswall(env.maze, s, :down) && (m[(i-1)*px+1:i*px, j*px] .= -3)
+            iswall(env.maze, s, :up) && (m[(i-1)*px+1:i*px, (j-1)*px+1] .= -3)
+            iswall(env.maze, s, :right) && (m[i*px, (j-1)*px+1:j*px] .= -3)
+            iswall(env.maze, s, :left) && (m[(i-1)*px+1, (j-1)*px+1:j*px] .= -3)
+            if s == env.mdp.state
+                val = -1
+            elseif s in goals
+                val = 1
+            else
+                continue
+            end
+            m[(i-1)*px+2:i*px-1, (j-1)*px+2:j*px-1] .= val
+        end
+    end
+    imshow(m, clim = (-3, 3), colormap = 42)
 end
